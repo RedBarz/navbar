61993c8c3ee0ab45e7e0b8a26a33373def4762b6
unknown <barzcorp.fx@gmail.com>
2024-04-10 21:07:14 +0200

    update

--- app/components/layouts/navbar/desktop/NavbarDesktop.jsx
+++ app/components/layouts/navbar/desktop/NavbarDesktop.jsx
@@ -1,144 +1,220 @@
 "use client";
 
-import React, { useState } from "react";
-import { FaAngleRight } from "react-icons/fa";
+import React, { useEffect, useRef, useState } from "react";
+import { gsap } from "gsap";
+import SplitType from "split-type";
+import { FaAngleRight, FaAngleLeft } from "react-icons/fa";
 import { GrAppsRounded } from "react-icons/gr";
-import { LuNewspaper } from "react-icons/lu";
-import { LuWallet } from "react-icons/lu";
+import { LuNewspaper, LuWallet } from "react-icons/lu";
 import { SlBell } from "react-icons/sl";
 import { FaPlus } from "react-icons/fa6";
 
 const NavbarDesktop = () => {
+  const [hidden, setHidden] = useState("");
+  const [reverseHidden, setReverseHidden] = useState("hidden");
+  const [additionalSpaceWidth, setAdditionalSpaceWidth] = useState("256px");
   const [showAdditionalCode, setShowAdditionalCode] = useState(false);
-  const [additionalSpaceHeight, setAdditionalSpaceHeight] = useState("290px");
-  const [additionalSpaceWidth, setAdditionalSpaceWidth] = useState("104px");
-  const [flexDirection, setFlexDirection] = useState("items-center");
-  const [margin, setMargin] = useState("ml-0");
-  const [hidden, setHidden] = useState("hidden");
-  const [flexCol, setFlexCol] = useState("flex-col");
+  const [iconWidth, setIconWidth] = useState("");
+  const [iconHeight, setIconHeight] = useState("");
+  const [mainIcon, setMainIcon] = useState("w-[208px]");
+  const [right, setRight] = useState("-right-[218px]");
+  const [textCenter, setTextCenter] = useState("");
+  const [duration, setDuration] = useState("");
+  const animateRef = useRef(null);
 
-  const toggleAdditionalSpace = () => {
-    setShowAdditionalCode(!showAdditionalCode);
-    setAdditionalSpaceHeight(showAdditionalCode ? "290px" : "400px");
-  };
+  useEffect(() => {
+    const typeSplit = new SplitType(animateRef.current, {
+      types: "lines, words, chars",
+      tagName: "span",
+    });
+
+    gsap.from(animateRef.current.querySelectorAll(".char"), {
+      y: "100%",
+      opacity: 0,
+      duration: 0.5,
+      ease: "power1.out",
+      stagger: 0.1,
+    });
+
+    return () => {
+      typeSplit.revert();
+    };
+  }, []);
 
   const toggleAdditionalSpaceWidth = () => {
     setShowAdditionalCode(!showAdditionalCode);
-    setAdditionalSpaceWidth(showAdditionalCode ? "104px" : "256px");
-    setFlexDirection((prevDirection) =>
-      prevDirection === "items-center" ? "items-start" : "items-center"
+    setAdditionalSpaceWidth(showAdditionalCode ? "256px" : "104px");
+    setHidden((prevHidden) => (prevHidden === "" ? "hidden" : ""));
+    setReverseHidden((prevReverseHidden) =>
+      prevReverseHidden === "hidden" ? "" : "hidden"
     );
-    setMargin((prevMargin) => (prevMargin === "ml-0" ? "ml-8" : "ml-0"));
-    setHidden((prevHidden) => (prevHidden === "hidden" ? "" : "hidden"));
-    setFlexCol((prevFlexCol) =>
-      prevFlexCol === "flex-col" ? "flex-row" : "flex-col"
+    setIconWidth((prevIconWidth) => (prevIconWidth === "" ? "w-[60px]" : ""));
+    setIconHeight((prevIconHeight) =>
+      prevIconHeight === "" ? "h-[60px]" : ""
+    );
+    setMainIcon((prevMainIcon) =>
+      prevMainIcon === "w-[208px]" ? "w-[57px]" : "w-[208px]"
+    );
+    setRight((prevRight) =>
+      prevRight === "-right-[218px]" ? "-right-[66px]" : "-right-[218px]"
+    );
+    setTextCenter((prevTextCenter) =>
+      prevTextCenter === "" ? "text-center" : ""
+    );
+    setDuration((prevDuration) =>
+      prevDuration === "" ? "duration-300" : "duration-300"
     );
   };
 
   return (
     <nav
       style={{ width: additionalSpaceWidth }}
-      className="w-[104px] h-screen rounded-[28px] border border-transparent ml-10 border-gradient"
+      className={`${duration} rounded-[28px] w-[256px] border border-transparent h-screen border-gradient ml-2`}
+      
     >
-      <div className=" w-[100%] h-[100%] rounded-[28px] navbar-color navbar-blur-1 flex flex-col">
-        <div
-          className={`flex flex-col justify-center ${flexDirection} border-b border-b-[#cc8b8b21] pb-[15px]`}
-        >
-          <div
-            className={`w-[52px] h-[12px] flex flex-row justify-between ${margin} items-center mt-6 `}
-          >
-            <div className="rounded-full w-3 h-3 bg-[#FF5A52]" />
-            <div className="rounded-full w-3 h-3 bg-[#E6C02A]" />
-            <div className="rounded-full w-3 h-3 bg-[#53C22B]" />
+      <div className="w-full h-full rounded-[28px] navbar-color navbar-blur1 p-6 flex flex-col">
+        <div className="w-full h-[129px] border-b border-[#66252542] flex flex-col ">
+          <div className="flex flex-row justify-between w-[52px] h-3 pb-[39px]">
+            <div className="w-[12px] h-[12px] rounded-full bg-[#FF5A52] hover:bg-[#ff5b529a] cursor-pointer" />
+            <div className="w-[12px] h-[12px] rounded-full bg-[#E6C02A] hover:bg-[#e6c02a9a] cursor-pointer" />
+            <div className="w-[12px] h-[12px] rounded-full bg-[#53C22B] hover:bg-[#53C22B9a] cursor-pointer" />
           </div>
-          <div className="mt-[30px] flex flex-row justify-center w-full">
-            <img src="/assets/avatar.svg" alt="" />
-            <div className={`ml-3 ${hidden}`}>
-              <p className="uppercase text-white/40 text-[11px]">
-                Product designer
-              </p>
-              <p className="text-white">Shreya Babulkar</p>
-            </div>
-          </div>
-          <button
-            onClick={toggleAdditionalSpaceWidth}
-            className="border w-6 h-6 flex justify-center items-center border-[#662525] rounded-full text-[#B86E9F] absolute -right-[10px] top-[77px] bg-[#0f090cd3]"
-          >
-            <FaAngleRight size={16} />
-          </button>
-        </div>
-        <div
-          className="flex flex-col items-center justify-between mb-5"
-          style={{ height: additionalSpaceHeight }}
-        >
-          <div className={`flex flex-col items-center mt-4`}>
-            <p className="uppercase text-white/50 text-[11px] my-5">main</p>
-            <div className="flex flex-row items-center mb-5">
-              <button
-                onClick={toggleAdditionalSpace}
-                className=" w-[56px] h-[56px] flex justify-center items-center rounded-xl border border-[#6625254e] bg-[#cc8b8b33] hover:bg-[#cc8b8b97] hover:duration-200 active:duration-200 active:bg-[#b86e9f8c] text-white backdrop-blur-xl hover:backdrop-blur-xl active:backdrop-blur-xl"
-              >
-                <GrAppsRounded size={24} />
-              </button>
-              <p className={`${hidden} text-white ml-5`}>Dashboard</p>
-            </div>
-            <div className={`${hidden}`}>
-              {showAdditionalCode && (
-                <div
-                  className={`${hidden} h-[124px] w-[158px] border-gradient rounded-2xl border border-transparent relative -right-[100px]`}
-                >
-                  <div className="navbar-color navbar-blur-1 w-full h-full rounded-2xl flex flex-col items-center text-white/50">
-                    <div className="flex justify-start items-center w-[95%] h-[95%] pl-4 my-1 hover:text-white hover:rounded-[12px] hover:border-[#6625254e] hover:bg-[#cc8b8b97] hover:transition-colors active:duration-200 active:bg-[#b86e9f8c]">
-                      <a href="/">Project</a>
-                    </div>
-                    <div className="flex justify-start items-center w-[95%] h-[95%] pl-4 my-1 hover:text-white hover:rounded-[12px] hover:border-[#6625254e] hover:bg-[#cc8b8b97] hover:transition-colors hover:duration-200 active:duration-200 active:bg-[#b86e9f8c]">
-                      <a href="/">Revenue</a>
-                    </div>
-                    <div className="flex justify-start items-center w-[95%] h-[95%] pl-4 my-1 hover:text-white hover:rounded-[12px] hover:border-[#6625254e] hover:bg-[#CC8B8B97] hover:transition-colors hover:duration-200 active:duration-200 active:bg-[#b86e9f8c]">
-                      <a href="/">Insights</a>
-                    </div>
-                  </div>
-                </div>
-              )}
-            </div>
-          </div>
-          <div className="flex flex-row justify-between items-center h-[150px]">
+          <div className="flex flex-row justify-start items-center">
             <div>
-              <LuWallet size={24} />
-              <LuNewspaper size={24} />
-              <SlBell size={24} />
+              <img src="/assets/avatar.svg" alt="" />
+            </div>
+            <div className="ml-2">
+              <p className={`${hidden} text-white/60 uppercase text-[11px]`}>
+                Product Designer
+              </p>
+              <p className={`${hidden} zoom-text text-white text-[14px]`} ref={animateRef}>
+                Shreya Babulkar
+              </p>
+            </div>
+          </div>
+
+          <div className={`${right} relative -right-[218px] bottom-9`}>
+            <button
+              onClick={toggleAdditionalSpaceWidth}
+              className={`${hidden} ${duration} text-[#AB638C] bg-[#0f090cd3] backdrop-blur-3xl rounded-full border border-[#662525]`}
+            >
+              <FaAngleLeft size={24} />
+            </button>
+            <button
+              onClick={toggleAdditionalSpaceWidth}
+              className={`${reverseHidden} ${duration} text-[#AB638C] bg-[#0f090cd3] backdrop-blur-3xl rounded-full border border-[#662525]`}
+            >
+              <FaAngleRight size={24} />
+            </button>
+          </div>
+        </div>
+        <div className={`${mainIcon} w-[208px] h-[382px] mt-5`}>
+          <p
+            className={`${textCenter} text-white/60 uppercase text-[11px] mb-[10px] ml-[6px]`}
+          >
+            Main
+          </p>
+          <div className="border-b border-[#66252542]">
+            <div
+              className={`${iconWidth} ${iconHeight} ${duration}  mb-[10px] flex flex-row justify-start py-4 px-5 rounded-xl items-center border border-[#6625254e] shadow-xl bg-[#cc8b8b33] hover:bg-[#cc8b8b97] hover:duration-200 active:duration-200 active:bg-[#b86e9f8c] text-white backdrop-blur-xl hover:backdrop-blur-xl active:backdrop-blur-xl hover:cursor-pointer `}
+            >
+              <GrAppsRounded size={32} />
+              <p className={`${hidden} pl-[10px]`}>Dashboard</p>
             </div>
             <div
-              className={`${hidden} text-white/50 hover:text-white hover:duration-150 hover:cursor-pointer flex flex-col justify-start items-star`}
+              className={`${iconWidth} ${iconHeight} ${duration} mb-[10px] flex flex-row justify-start py-4 px-5 rounded-xl items-center border border-[#6625254e] shadow-xl bg-[#cc8b8b33] hover:bg-[#cc8b8b97] hover:duration-200 active:duration-200 active:bg-[#b86e9f8c] text-white backdrop-blur-xl hover:backdrop-blur-xl active:backdrop-blur-xl hover:cursor-pointer `}
             >
-              <p>Contracts</p>
-              <p>Payment</p>
-              <p>Notification</p>
+              <LuNewspaper size={32} />
+              <p className={`${hidden} pl-[10px]`}>Contracts</p>
+            </div>
+            <div
+              className={`${iconWidth} ${iconHeight} ${duration} mb-[10px] flex flex-row justify-start py-4 px-5 rounded-xl items-center border border-[#6625254e] shadow-xl bg-[#cc8b8b33] hover:bg-[#cc8b8b97] hover:duration-200 active:duration-200 active:bg-[#b86e9f8c] text-white backdrop-blur-xl hover:backdrop-blur-xl active:backdrop-blur-xl hover:cursor-pointer `}
+            >
+              <LuWallet size={32} />
+              <p className={`${hidden} pl-[10px]`}>Payments</p>
+            </div>
+            <div
+              className={`${iconWidth} ${iconHeight} ${duration} mb-[10px] flex flex-row justify-start py-4 px-5 rounded-xl items-center border border-[#6625254e] shadow-xl bg-[#cc8b8b33] hover:bg-[#cc8b8b97] hover:duration-200 active:duration-200 active:bg-[#b86e9f8c] text-white backdrop-blur-xl hover:backdrop-blur-xl active:backdrop-blur-xl hover:cursor-pointer `}
+            >
+              <SlBell size={32} />
+              <p className={`${hidden} pl-[10px]`}>Notifications</p>
             </div>
           </div>
         </div>
-        <div className="flex justify-between flex-col items-center h-full mb-10">
-          <div className="flex flex-col items-center border-t border-t-[#cc8b8b21] pt-5">
-            <p className="text-white/40 text-[11px] uppercase">team me...</p>
-            <p className="text-white/40 text-[11px] uppercase">team members</p>
-            <div className="h-[196px] flex flex-col justify-around">
-              <div>
+        <div className="flex flex-col justify-around h-full">
+          <div className="w-full flex flex-row justify-between items-center">
+            <p
+              className={`${reverseHidden}  uppercase text-white/60 text-[11px] ml-2`}
+            >
+              team me...
+            </p>
+            <p
+              className={` ${hidden} uppercase text-white/60 text-[11px] ml-2 mt-[10px]`}
+            >
+              Team members
+            </p>
+            <button
+              className={`${hidden} text-white/60 hover:text-white mt-[7px]`}
+            >
+              <FaPlus size={12} />
+            </button>
+          </div>
+          <div>
+            <div className="flex flex-row items-center justify-start">
+              <div className="py-4 px-2">
                 <img src="/assets/profil1.svg" alt="" />
                 <div className="w-[6px] h-[6px] bg-[#7FBA7A] rounded-full relative bottom-2 -right-5" />
               </div>
-              <div>
+              <p
+                className={`${hidden} text-white/60 ml-[10px] hover:text-white cursor-pointer`}
+              >
+                Esther Howard
+              </p>
+            </div>
+            <div className="flex flex-row items-center justify-start">
+              <div className="py-4 px-2">
                 <img src="/assets/profil2.svg" alt="" />
                 <div className="w-[6px] h-[6px] bg-[#250D0E] rounded-full border border-[#9F9595] relative bottom-2 -right-5" />
               </div>
-              <div>
+              <p
+                className={`${hidden} text-white/60 ml-[10px] hover:text-white cursor-pointer`}
+              >
+                Jacob Jones
+              </p>
+            </div>
+            <div className="flex flex-row items-center justify-start">
+              <div className="py-4 px-2">
                 <img src="/assets/profil3.svg" alt="" />
                 <div className="w-[6px] h-[6px] bg-[#7FBA7A] rounded-full relative bottom-2 -right-5" />
               </div>
+              <p
+                className={`${hidden} text-white/60 ml-[10px] hover:text-white cursor-pointer`}
+              >
+                Cody Fisher
+              </p>
             </div>
           </div>
-          <button className=" backdrop-blur-xl shadow-lg shadow-[#a8510530] w-[48px] h-[48px] bg-[#E0822D] rounded-xl flex justify-center items-center text-white">
-            <FaPlus size={20} />
-          </button>
+          <div>
+            <button
+              className={`${reverseHidden} text-white flex flex-row items-center justify-around w-[48px] h-[48px] bg-[#E0822D] hover:bg-[#e0802db6] shadow-md shadow-[#e0802d45] rounded-xl`}
+            >
+              <FaPlus size={14} />
+            </button>
+          </div>
+        </div>
+        <div
+          className={`${hidden} w-[208px] h-[181px] border-gradient rounded-[28px]`}
+        >
+          <div className="w-[208px] h-[181px] navbar-color border border-transparent rounded-[28px] p-4 flex flex-col justify-around">
+            <p className="text-white text-[16px] text-center">Let's start!</p>
+            <p className="text-white/60 text-[13px] text-center">
+              Creating or adding new tasks couldn't be easier
+            </p>
+            <button className="text-white flex flex-row items-center justify-around w-full px-4 py-[11px] bg-[#E0822D] hover:bg-[#e0802db6] shadow-md shadow-[#e0802d45] rounded-xl">
+              <FaPlus size={14} />
+              <p className="text-[14px]">Add New Task</p>
+            </button>
+          </div>
         </div>
       </div>
     </nav>
